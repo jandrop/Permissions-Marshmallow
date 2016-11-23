a592dc03da8c102a06f15af0ee0858ea5906b049
Alejandro Platas Mallo <alejandro.platas@mobgen.com>
2016-11-23 15:38:45 +0100

    [Feature]: Add a string array of permissions

--- app/src/main/java/es/atrapandocucarachas/permissionsm/MainActivity.java
+++ app/src/main/java/es/atrapandocucarachas/permissionsm/MainActivity.java
@@ -9,29 +9,33 @@ import es.atrapandocucarachas.permissionsm.helpers.PermissionHelper;
 
 public class MainActivity extends AppCompatActivity implements PermissionHelper.PermissionCallback {
 
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_main);
 
-        PermissionHelper permissionHelper = new PermissionHelper(this, this);
-        if (!permissionHelper.isPermissionGranted(Manifest.permission.ACCESS_FINE_LOCATION)) {
-            permissionHelper.requestPermission(Manifest.permission.ACCESS_FINE_LOCATION);
-        }
-    }
+		PermissionHelper permissionHelper = new PermissionHelper(this, this);
+		if (!permissionHelper.isPermissionGranted(Manifest.permission.ACCESS_FINE_LOCATION)) {
+			String[] permissions = new String[3];
+			permissions[0] = Manifest.permission.ACCESS_FINE_LOCATION;
+			permissions[1] = Manifest.permission.CAMERA;
+			permissions[2] = Manifest.permission.READ_CONTACTS;
+			permissionHelper.requestPermissions(permissions);
+		}
+	}
 
 
-    @Override
-    public void onPermissionGranted(String permission) {
-        Toast.makeText(MainActivity.this, permission, Toast.LENGTH_SHORT).show();
-        Toast.makeText(MainActivity.this, "Permission Granted", Toast.LENGTH_SHORT).show();
-    }
+	@Override
+	public void onPermissionGranted(String permission) {
+		Toast.makeText(MainActivity.this, permission, Toast.LENGTH_SHORT).show();
+		Toast.makeText(MainActivity.this, "Permission Granted", Toast.LENGTH_SHORT).show();
+	}
 
-    @Override
-    public void onPermissionDenied(String permission) {
-        Toast.makeText(MainActivity.this, permission, Toast.LENGTH_SHORT).show();
-        Toast.makeText(MainActivity.this, "You have denied the permission so if you want to access the location, " +
-                "you will activate the permission manually", Toast.LENGTH_SHORT).show();
+	@Override
+	public void onPermissionDenied(String permission) {
+		Toast.makeText(MainActivity.this, permission, Toast.LENGTH_SHORT).show();
+		Toast.makeText(MainActivity.this, "You have denied the permission so if you want to access the location, " +
+				"you will activate the permission manually", Toast.LENGTH_SHORT).show();
 
-    }
+	}
 }
--- app/src/main/java/es/atrapandocucarachas/permissionsm/helpers/PermissionHelper.java
+++ app/src/main/java/es/atrapandocucarachas/permissionsm/helpers/PermissionHelper.java
@@ -8,69 +8,72 @@ import android.support.v4.content.PermissionChecker;
 
 public class PermissionHelper implements ActivityCompat.OnRequestPermissionsResultCallback {
 
-    private static final int PERMISSION_REQ_CODE = 324;
+	private static final int PERMISSION_REQ_CODE = 324;
 
-    private final Activity context;
-    private final PermissionCallback callback;
+	private final Activity context;
+	private final PermissionCallback callback;
 
-    public PermissionHelper(@NonNull Activity context, @NonNull PermissionCallback callback) {
-        this.context = context;
-        this.callback = callback;
-    }
+	public PermissionHelper(@NonNull Activity context, @NonNull PermissionCallback callback) {
+		this.context = context;
+		this.callback = callback;
+	}
 
-    /**
-     * Checks if the given permission has been granted by the user to this application before.
-     *
-     * @param permission to check availability for
-     * @return true if the permission is currently granted, false otherwise.
-     */
-    public boolean isPermissionGranted(@NonNull String permission) {
-        int result = ContextCompat.checkSelfPermission(context, permission);
-        return result == PermissionChecker.PERMISSION_GRANTED;
-    }
+	/**
+	 * Checks if the given permission has been granted by the user to this application before.
+	 *
+	 * @param permission to check availability for
+	 * @return true if the permission is currently granted, false otherwise.
+	 */
+	public boolean isPermissionGranted(@NonNull String permission) {
+		int result = ContextCompat.checkSelfPermission(context, permission);
+		return result == PermissionChecker.PERMISSION_GRANTED;
+	}
 
-    /**
-     * Starts the async request of the given permission.
-     *
-     * @param permission to request to the user
-     */
-    public void requestPermission(@NonNull String permission) {
-        if (ActivityCompat.shouldShowRequestPermissionRationale(context, permission)) {
-            //Show explanation dialog.
-            //Optionally, call requestPermission again on positive button click
-        } else {
-            ActivityCompat.requestPermissions(context,
-                    new String[]{permission},
-                    PERMISSION_REQ_CODE);
-        }
-    }
+	/**
+	 * Starts the async request of the given permission.
+	 *
+	 * @param permission to request to the user
+	 */
+	public void requestPermission(@NonNull String permission) {
+		ActivityCompat.requestPermissions(context, new String[]{permission}, PERMISSION_REQ_CODE);
+	}
 
-    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
-        if (requestCode != PERMISSION_REQ_CODE || (permissions.length == 0 && grantResults.length == 0)) {
-            return;
-        }
-        for (int i = 0; i < permissions.length; i++) {
-            if (grantResults[i] == PermissionChecker.PERMISSION_GRANTED) {
-                callback.onPermissionGranted(permissions[i]);
-            } else {
-                callback.onPermissionDenied(permissions[i]);
-            }
-        }
-    }
+	/**
+	 * Starts the async request of the given permissions
+	 *
+	 * @param permissions to request to the user
+	 */
+	public void requestPermissions(@NonNull String[] permissions) {
+		ActivityCompat.requestPermissions(context, permissions, PERMISSION_REQ_CODE);
 
-    public interface PermissionCallback {
-        /**
-         * Called when a permission request is accepted by the user.
-         *
-         * @param permission the user just accepted.
-         */
-        void onPermissionGranted(String permission);
+	}
 
-        /**
-         * Called when a permission request is declined by the user.
-         *
-         * @param permission the user just declined.
-         */
-        void onPermissionDenied(String permission);
-    }
+	public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+		if (requestCode != PERMISSION_REQ_CODE || (permissions.length == 0 && grantResults.length == 0)) {
+			return;
+		}
+		for (int i = 0; i < permissions.length; i++) {
+			if (grantResults[i] == PermissionChecker.PERMISSION_GRANTED) {
+				callback.onPermissionGranted(permissions[i]);
+			} else {
+				callback.onPermissionDenied(permissions[i]);
+			}
+		}
+	}
+
+	public interface PermissionCallback {
+		/**
+		 * Called when a permission request is accepted by the user.
+		 *
+		 * @param permission the user just accepted.
+		 */
+		void onPermissionGranted(String permission);
+
+		/**
+		 * Called when a permission request is declined by the user.
+		 *
+		 * @param permission the user just declined.
+		 */
+		void onPermissionDenied(String permission);
+	}
 }
